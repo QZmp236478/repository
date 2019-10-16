e673a74d1e61e9cd43bf45eabd88d1e4413ca044
lp <1269735772@qq.com>
2019-10-16 14:41:04 +0800

    测试项目

---
+++ src/test/java/com/resp/resp/RespApplicationTests.java
@@ -0,0 +1,16 @@
+package com.resp.resp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RespApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
